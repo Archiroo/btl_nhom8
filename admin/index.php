0f36ca0754b4d47ef89331a4e43597678d6b0bf5
Archiroo <newbieit.nb@gmail.com>
2022-01-04 08:32:56 +0700

    Xong dăng nhập

--- admin/index.php
+++ admin/index.php
@@ -1,20 +1,16 @@
 <?php
     include('header.php');
 ?>
-    <main>
+    <!-- <main>
         <h2 class="dash-title">Overiew</h2>
         <div class="dash-cards">
             <div class="card-single">
                 <div class="card-body">
                     <span class="fas fa-user-graduate"></span>
                     <div>
-                        <?php
-                            $sql = "SELECT * FROM tb_student";
-                            $res = mysqli_query($conn, $sql);
-                            $count = mysqli_num_rows($res);
-                        ?>
+                        
                         <h5>Student</h5>
-                        <h4><?php echo $count; ?></h4>
+                        <h4></h4>
                         <a href="student.php">
                             View all
                         </a>
@@ -25,13 +21,9 @@
                 <div class="card-body">
                     <span class="fas fa-book-open"></span>
                     <div>
-                        <?php
-                            $sql1 = "SELECT * FROM tb_subject";
-                            $res1 = mysqli_query($conn, $sql1);
-                            $count1 = mysqli_num_rows($res1);
-                        ?>
+                        
                         <h5>Subject</h5>
-                        <h4><?php echo $count1; ?></h4>
+                        <h4></h4>
                         <a href="subject.php">
                             View all
                         </a>
@@ -43,12 +35,8 @@
                     <span class="fas fa-user-tie"></span>
                     <div>
                         <h5>Homework</h5>
-                        <?php
-                            $sql2 = "SELECT * FROM tb_homework where home_level=0";
-                            $res2 = mysqli_query($conn, $sql2);
-                            $count2 = mysqli_num_rows($res2);
-                        ?>
-                        <h4><?php echo $count2;?></h4>
+                        
+                        <h4></h4>
                         <a href="homework.php">
                             View all
                         </a>
@@ -59,13 +47,9 @@
                 <div class="card-body">
                     <span class="fas fa-folder"></span>
                     <div>
-                        <?php
-                            $sql3 = "SELECT * FROM tb_homework where home_level=1";
-                            $res3 = mysqli_query($conn, $sql2);
-                            $count3 = mysqli_num_rows($res3);
-                        ?>
+                        
                         <h5>Projects</h5>
-                        <h4><?php echo $count3;?></h4>
+                        <h4></h4>
                         <a href="project.php">
                             View all
                         </a>
@@ -91,66 +75,23 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                <?php 
-                                    $sql4 = "SELECT * FROM tb_team, tb_homework, tb_mark, tb_student 
-                                            WHERE tb_team.id_homework = tb_homework.id_homework and home_level = 1
-                                            and tb_mark.id_student = tb_student.id_student and tb_homework.id_homework = tb_mark.id_homework";
-                                    $res4 = mysqli_query($conn, $sql4);
-                                    if($res4 == TRUE)
-                                    {
-                                        $count4 = mysqli_num_rows($res4);
-                                        if($count4>0){
-                                            while($row4 = mysqli_fetch_assoc($res4))
-                                            {
-                                                $id_team = $row4['id_team'];
-                                                $name_project = $row4['name_homework'];
-                                                $start_date = $row4['start_date'];
-                                                $end_date = $row4['end_date'];
-                                                $finish_date = $row4['finish_date'];
-                                                $img_std = $row4['image_student'];
-                                ?>
+                                
                                                 <tr>
-                                                    <td><?php echo $id_team;?></td>
-                                                    <td><?php echo $name_project?></td>
-                                                    <td><?php echo $start_date;?></td>
-                                                    <td><?php echo $end_date;?></td>
-                                                    <td><?php echo $finish_date;?></td>
+                                                    
                                                     <td class="td-team" style="margin-left: 2.5rem;">
                                                         <div class="img-1">
-                                                            <img src="../image/<?php echo $img_std;?>" alt="">
+                                                            <img src="../image/" alt="">
                                                         </div>
                                                     </td>
                                                     <td>
-                                                        <?php 
-                                                            $date1 = strtotime($end_date);
-                                                            $date2 =  strtotime($finish_date);
-                                                            $date3 = $date2 - $date1;
-                                                            if($date3>0)
-                                                            {
-                                                                ?>
+                                                        
                                                                     <span class="badge warning">Late</span>
-                                                                <?php
-                                                            }
-                                                            else
-                                                            {
-                                                                ?>
+                                                                
                                                                     <span class="badge success">Success</span>
-                                                                <?php 
-                                                            }
-                                                        ?>
+                                                        
                                                     </td>
                                                 </tr>
-                                <?php
-                                            }
-                                        }
-                                        else
-                                        {
-
-                                        }
-                                    }
-                                        
-                                    ?>
-                                <!-- <tr>
+                          <tr>
                                     <td>3</td>
                                     <td>Logo Design</td>
                                     <td>15 Aug, 2020</td>
@@ -160,12 +101,12 @@
                                         <span class="badge warning">Processing</span>
                                     </td>
                                 </tr> -->
-                            </tbody>
+                            <!-- </tbody>
                         </table>
                     </div>
                 </div>
             </div>
-        </section>
+        </section> --> -->
 
 <?php
     include('footer.php');
---
+++ login.php
@@ -0,0 +1,32 @@
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css">
+    <link rel="stylesheet" href="css/login.css">
+    <title>Log-in</title>
+</head>
+
+<body>
+    <div class="login">
+        
+        <form action="" method="POST">
+            <h3>ĐĂNG NHẬP</h3>
+            <span>Tài khoản</span>
+            <input type="text" class="box" name="username" placeholder="Tên tài khoản">
+            <span>Mật khẩu</span>
+            <input type="password" class="box" name="password" placeholder="Mật khẩu">
+            <input type="submit" value="Đăng nhập" class="btn" name="submit">
+            <p>Quên mật khẩu ? <a href="#">Bấm vào đây.</a></p>
+        </form>
+    </div>
+</body>
+
+</html>
+
+
+
+
+

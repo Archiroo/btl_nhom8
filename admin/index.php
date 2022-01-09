df3801a60142f948b76aadfbafad945f377486a9
Archiroo <newbieit.nb@gmail.com>
2022-01-09 07:34:07 +0700

    abc

--- admin/index.php
+++ admin/index.php
@@ -8,7 +8,7 @@ include('header.php');
                 <span class="fas fa-user-friends"></span>
                 <div>
 
-                    <h5>QL nhân viên</h5>
+                    <h5>Quản lý nhân viên</h5>
                     <h4></h4>
                     <a href="./employee.php">
                         Xem tất cả
@@ -21,7 +21,7 @@ include('header.php');
                 <span class="fas fa-folder"></span>
                 <div>
 
-                    <h5>QL thực đơn</h5>
+                    <h5>Quản lý thực đơn</h5>
                     <h4></h4>
                     <a href="./thucdon.php">
                         Xem tất cả
@@ -68,9 +68,9 @@ include('header.php');
                             <tr>
                                 <th>STT</th>
                                 <th>Khách hàng</th>
-                                <th>Số điện thoại</th>
-                                <th>Món ăn</th>
-                                <th>Giá tiền</th>
+                                <th>Điện thoại</th>
+                                <th>Địa chỉ</th>
+                                <th>Hóa đơn</th>
                                 <th>Ngày hóa đơn</th>
                                 <th>Nhân viên bán</th>
                                 <th>Trạng thái</th>
@@ -81,9 +81,9 @@ include('header.php');
                         <td>1</td>
                         <td>Đào Văn Tân</td>
                         <td>012 333 3333</td>
-                        <td>Pizza hải sản</td>
-                        <td>129.000 VNĐ</td>
-                        <td style="text-align: center">6/1/2022</td>
+                        <td>Ninh Bình</td>
+                        <td class="text_center">HD001</td>
+                        <td class="text_center">6/1/2022</td>
                         <td>Nguyễn Minh Đức</td>
                         <td class="hoanthanh">Hoàn thành</td>
                         
@@ -92,9 +92,9 @@ include('header.php');
                         <td>2</td>
                         <td>Hồ Hồng Quân</td>
                         <td>012 444 4444</td>
-                        <td>Pizza hải sản</td>
-                        <td>129.000 VNĐ</td>
-                        <td>6/1/2022</td>
+                        <td>Hà Nội</td>
+                        <td class="text_center">HD002</td>
+                        <td class="text_center">6/1/2022</td>
                         <td>Nguyễn Minh Đức</td>
                         <td class="hoanthanh">Hoàn thành</td>
                         
@@ -103,9 +103,9 @@ include('header.php');
                         <td>3</td>
                         <td>Nguyễn Vương</td>
                         <td>012 555 5555</td>
-                        <td>Pizza hải sản</td>
-                        <td>129.000</td>
-                        <td>6/1/2022</td>
+                        <td>Đống Đa</td>
+                        <td class="text_center">HD003</td>
+                        <td class="text_center">6/1/2022</td>
                         <td>Nguyễn Minh Đức</td>
                         <td class="huy">Hủy</td>
                         
@@ -114,9 +114,9 @@ include('header.php');
                         <td>4</td>
                         <td>Trịnh Duy Long</td>
                         <td>012 666 6666</td>
-                        <td>Pizza hải sản</td>
-                        <td>129.000</td>
-                        <td>6/1/2022</td>
+                        <td>Long Biên</td>
+                        <td class="text_center">HD004</td>
+                        <td class="text_center">6/1/2022</td>
                         <td>Nguyễn Minh Đức</td>
                         <td class="hoanthanh">Hoàn thành</td>
                         
@@ -125,9 +125,9 @@ include('header.php');
                         <td>5</td>
                         <td>Hoàng Trang</td>
                         <td>012 777 7777</td>
-                        <td>Pizza hải sản</td>
-                        <td>129.000</td>
-                        <td>6/1/2022</td>
+                        <td>Chương Mỹ</td>
+                        <td class="text_center">HD005</td>
+                        <td class="text_center">6/1/2022</td>
                         <td>Nguyễn Minh Đức</td>
                         <td class="huy">Hủy</td>
                         
@@ -136,9 +136,9 @@ include('header.php');
                         <td>6</td>
                         <td>Tuấn Anh</td>
                         <td>012 888 8888</td>
-                        <td>Pizza hải sản</td>
-                        <td>129.000</td>
-                        <td>6/1/2022</td>
+                        <td>Hà Đông</td>
+                        <td class="text_center">HD006</td>
+                        <td class="text_center">6/1/2022</td>
                         <td>Nguyễn Minh Đức</td>
                         <td class="hoanthanh">Hoàn thành</td>
                         
